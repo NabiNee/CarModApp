e7c8137e00cbbeedede544321a64bb176fe1bdb0
AguaAdri <aguay005@cougars.csusm.edu>
2016-03-08 10:00:04 -0800

    Create Project.h

---
+++ Project.h
@@ -0,0 +1,10 @@
+class Projects{
+private:
+Listener l;
+Calculator calculator;
+Car car;
+
+public:
+Proect();
+User& saveProject(); //Saves project by returning updated user with project to the DB handler
+}
