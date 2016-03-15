c3ebf7240b31c0a823c86b5b711fe34391c99ee2
NabiNee <hom.bom@gmail.com>
2016-03-15 08:29:43 -0700

    Added files via upload

---
+++ Systems.cs
@@ -0,0 +1,29 @@
+/**************************************************************************
+Class: Systems
+Description: entry point for app
+
+*************************************************************************/
+Class Systems
+{
+	private: 
+		Systems()
+		{
+			init();
+		}
+		
+		void init()
+		{
+			// init DBHandler
+			// init GUIHandler
+			
+			// User Intro Screen
+			
+			// Get User save from DBHandler 
+			
+			// ELSE create new user and save to DBHandler
+			
+			// init Project
+			
+			//IF NEEDED: enter continues background task here
+		}
+}
