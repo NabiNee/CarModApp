ce7bd811d7295c279a7e48b78467bce842115687
NabiNee <hom.bom@gmail.com>
2016-03-15 08:24:14 -0700

    Added files via upload

---
+++ Project.cs
@@ -0,0 +1,41 @@
+/**************************************************************************
+Class: Project
+Description: Entry point and handler of the project system of app
+	designed to construct project from infomration given by user and
+	car varables.
+
+*************************************************************************/
+
+class Project extended Systems
+{
+	private:
+		User CurrentUser;
+		
+	
+	public:
+		// constructor
+		Project()
+		{
+			init();
+		}
+		
+		
+		init()
+		{
+			// init the User data struck
+			
+			
+			// init the calculator
+
+			
+			// init the car modifyer
+			
+			
+			// registrate the listeners
+		
+		
+		}
+
+		// save current user info
+		SaveUserInfo();
+}
