acddfda8aca48b4e29dfda7e7a1b10f26cb5f30b
AguaAdri <aguay005@cougars.csusm.edu>
2016-03-17 10:16:32 -0700

    Update Calculator.h
    we added functions to get the parameters quickly, and functions to update the prices and budget. The updateTotal function was written with the assumption that the Project class has a vector containing all the parts used. If this is incorrect, please change it.

--- Calculator.h
+++ Calculator.h
@@ -1,7 +1,11 @@
-class Calculator{
+const float TAX = 0.08;
+public class Calculator : Projects
+{
 private:
 float total; //current total of the project
 float budgetAmount; //max amount of project cost
+float grandTotal; //includes tax and shipping
+float taxTotal; //the tax from the total
 public:
 Calculator(){ //initilaizes everything to 0
   total = 0;
@@ -11,6 +15,39 @@ Calculator(float budget){ //initializes calculator with budgetAmount = budget
   total = 0;
   budgetAmount = budget;
 }
-float updateTotal(float){ //updates the amount total using listeners?
+void updateTotal(){ //updates the amount total by grabbing vector (?) from project class
+  for(int counter = 0; counter < Project:: carPart.size(); counter ++)
+  {
+    total += Project::carParts.price;
+  }
+}
+void updateTaxTotal()
+{
+  taxTotal = total * TAX;
+}
+void calculateGrandTotal()
+{
+  grandTotal = (total * TAX) + total;
+}
+void updateBudget(){ // updating budget
+  budgetAmount = budgetAmount - total;
+}
+  float budget(){ //returns the budget in red if it's passed the budget amount
+      if (budgetAmount > 0){
+        return budgetAmount;
+      }
+        else{
+            return budgetAmount; //in some red color
+        }
+    }
+  }
+float getTotal{
+  return total;
+}
+float getTaxTotal{
+  return taxTotal;
+}
+float getGrandTotal{
+  return grandTotal;
 }
 };
