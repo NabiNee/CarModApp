5b686f2e512e0779201a9bf0fdf2f3c5cc869adf
AguaAdri <aguay005@cougars.csusm.edu>
2016-03-17 09:09:40 -0700

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -3,7 +3,14 @@ private:
 float total; //current total of the project
 float budgetAmount; //max amount of project cost
 public:
-Calculator(); //initilaizes everything to 0
-Calculator(float budget); //initializes calculator with budgetAmount = budget
-float updateTotal(float); 
+Calculator(){ //initilaizes everything to 0
+  total = 0;
+  budgetAmount = 0;
+}
+Calculator(float budget){ //initializes calculator with budgetAmount = budget
+  total = 0;
+  budgetAmount = budget;
+}
+float updateTotal(float){ //updates the amount total using listeners?
+}
 };
