46da5498bc79e24b801465ea0e101cb60d8ca3a0
AguaAdri <aguay005@cougars.csusm.edu>
2016-04-19 09:50:23 -0700

    Update System.h

--- System.h
+++ System.h
@@ -1,11 +1,35 @@
-//System header file
-class System
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <QMainWindow>
+#include "userlist.h"
+#include "projects.h"
+#include "createaccount.h"
+
+namespace Ui {
+class System;
+}
+
+class System : public QMainWindow
 {
-public: //initializes objects 
-Database db;
-Tutorial tutorial;
-Forums forums;
-Projects projects;
+    Q_OBJECT
+
+public:
+    explicit System(QWidget *parent = 0);
+    ~System();
+
+private slots:
+    void on_buttonCreateAccount_clicked();
+
+    void on_buttonLogIn_clicked();
+
+    void on_buttonSkip_clicked();
 
 private:
+    Ui::System *ui;
+    Projects project;
+    UserList list; //an object containg list of users; should be replaced with database
+    CreateAccount newAccount;
 };
+
+#endif // SYSTEM_H
